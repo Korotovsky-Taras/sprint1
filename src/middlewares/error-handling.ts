3b512fe531eaabb25ca8b5604f686c26765e2e4e
taras <regeidt.my@gmail.com>
2023-06-25 17:28:14 +0300

    home task 1 fix2

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -3,13 +3,12 @@ import {NextFunction, Request, Response} from "express";
 
 const ErrorHandling = (err: Error, req: Request, res: Response, next: NextFunction) =>  {
     if (err instanceof ApiError) {
-        let response = res.status(err.status);
         if (err.errors) {
-            return response.json({
+            return res.status(err.status).json({
                 errorsMessages: err.errors,
             })
         }
-        return response.send();
+        return res.status(err.status).send();
     }
     return res.status(500).json({ message: "Unhandled error" });
 };
